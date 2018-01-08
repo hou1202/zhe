e774edf953cb1b62b67c198d551d03d8fa1a6340
houss@aliyun.com <houss@aliyun.com>
2018-01-08 19:43:18 +0800

    database

--- application/database.php
+++ application/database.php
@@ -13,15 +13,15 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    //'hostname'        => '139.224.164.117',
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '120.55.44.2',
+    //'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'zhe',
     // 用户名
     'username'        => 'root',
     // 密码
-    //'password'        => 'kejiXiaoji2016',
-    'password'        => 'root',
+    'password'        => 'che12345',
+    //'password'        => 'root',
 
     // 端口
     'hostport'        => '',
