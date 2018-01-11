4d6caabd7301a21c83255ef5cf21af5200b1d8c1
houss@aliyun.com <houss@aliyun.com>
2018-01-11 15:24:55 +0800

    goods

--- application/index/controller/Goods.php
+++ application/index/controller/Goods.php
@@ -25,6 +25,7 @@ class Goods extends Controller {
 
     /*
     * @goodsDetails()  产品详情
+     * $id 通过GET方式传过来的产品id
     * */
     public function goodsDetails(){
         if(isset($_GET['id']) && !empty($_GET['id'])){
@@ -32,9 +33,136 @@ class Goods extends Controller {
             $goods = new GoodModel();
             $getOne = $goods -> getGoodsDetailsById($id);
             $Recommend = $goods ->getRecommendGoodsById($id);
-            return $getOne ?  view('goods/details',['getOne' => $getOne,'Recommend' => $Recommend]) : ReturnJson::ReturnA("未获取到相应的产品信息...");
+            return $getOne ?  $this -> fetch('goods/details',['getOne' => $getOne,'Recommend' => $Recommend]) : ReturnJson::ReturnA("未获取到相应的产品信息...");
         }else{
-            return ReturnJson::ReturnA("无效的修改操作...");
+            return ReturnJson::ReturnA("未获取到相应的产品信息...");
         }
+    }
+
+    /*
+     * @ todayCouponGoodsList() 今日优惠券列表
+     * * */
+    public function todayCouponGoodsList(){
+        $goods = new GoodModel();
+        $goodsList = $goods -> getGoodsListByTime();
+        return $this -> fetch('goods/block-goods',['List' => $goodsList]);
+
+    }
+
+    /*
+    * @ tmallCouponGoodsList() 天猫专属优惠券列表
+    * * */
+    public function tmallCouponGoodsList(){
+        $goods = new GoodModel();
+        $goodsList = $goods -> getGoodsListByTmall();
+        return $this -> fetch('goods/block-goods',['List' => $goodsList]);
+
+    }
+
+    /*
+    * @ moneyCouponGoodsList() 大额优惠券列表
+    * * */
+    public function moneyCouponGoodsList(){
+        $goods = new GoodModel();
+        $goodsList = $goods -> getGoodsListByCouponMoney();
+        return $this -> fetch('goods/block-goods',['List' => $goodsList]);
+
+    }
+
+    /*
+    * @ nineCouponGoodsList() 9.9优惠券列表
+    * * */
+    public function nineCouponGoodsList(){
+        $goods = new GoodModel();
+        $goodsList = $goods -> getGoodsListByNine();
+        return $this -> fetch('goods/block-goods',['List' => $goodsList]);
+
+    }
+
+    /*
+    * @ ratioCouponGoodsList() 高奖金优惠券列表
+    * * */
+    public function ratioCouponGoodsList(){
+        $goods = new GoodModel();
+        $goodsList = $goods -> getGoodsListByRatio();
+        return $this -> fetch('goods/block-goods',['List' => $goodsList]);
+
+    }
+
+    /*
+    * @ perfectCouponGoodsList() 精选好券列表
+    * * */
+    public function perfectCouponGoodsList(){
+        $goods = new GoodModel();
+        $goodsList = $goods -> getPerfectCoupon();
+        return $this -> fetch('goods/strip-goods',['List' => $goodsList]);
+
+    }
+
+    /*
+    * @ HotCouponGoodsList() 爆款好券列表
+    * * */
+    public function HotCouponGoodsList(){
+        $goods = new GoodModel();
+        $goodsList = $goods -> getHotCoupon();
+        return $this -> fetch('goods/strip-goods',['List' => $goodsList]);
+
+    }
+
+    /*
+    * @ couponSquare() 券广场
+    * * */
+    public function couponSquare(){
+        $goods = new GoodModel();
+        $goodsList = $goods -> getCouponSquare();
+        return $this -> fetch('other/square',['List' => $goodsList]);
+
+    }
+
+    /*
+  * @ navCouponList() 导航优惠券列表
+  * * */
+    public function navCouponList(){
+        //return $this -> fetch('other/square',['List' => $goodsList]);
+        if(isset($_GET['nav']) && !empty($_GET['nav'])){
+            $nav = $this -> request -> get('nav');
+            $goods = new GoodModel();
+            $goodsList = $goods -> getCouponNav($nav);
+            return $goodsList ?  $this -> fetch('goods/strip-goods',['List' => $goodsList]) : ReturnJson::ReturnA("未获取到相应的产品信息...");
+        }else{
+            $this -> redirect('/index/goods/couponSquare');
+        }
+
+    }
+
+
+
+
+
+    /*
+     * @ goodsList() 产品列表
+     * $type 产品列表形式
+     *      默认为条状产品列表
+     *      $type为空则为条状产品列表
+     *      1=》 条状产品列表
+     *      2=》 块状产品列表
+     * * */
+    public function goodsList(){
+        if(isset($_GET['type']) && !empty($_GET['type'])){
+            $type = $this -> request -> get('type');
+            if($type == 1){
+
+                return $this -> fetch('goods/strip-goods');
+            }elseif($type == 2){
+
+                return $this -> fetch('goods/block-goods');
+            }else{
+
+                return $this -> fetch('goods/strip-goods');
+            }
+        }else{
+            return $this -> fetch('goods/strip-goods');
+        }
+
     }
 }
--- application/index/model/Goods.php
+++ application/index/model/Goods.php
@@ -18,23 +18,49 @@ class Goods extends Model
         return $value = sprintf("%1\$.2f",$value*0.3);
     }
 
+    protected function getTypeAttr($value){
+        $type = ['淘宝' => '/static/index/images/b-logo.png','天猫' => '/static/index/images/t-logo.png'];
+        return $type[$value];
+    }
+
     /*
      * @getIndexPerfectCoupon() 首页精选好券20个
      * */
     public function getIndexPerfectCoupon(){
-        return $this ->field('id,goods_id,name,banner,price,coupon_money')
+        return $this ->field('id,goods_id,name,banner,price,sales,type,coupon_money')
                     -> order('commission DESC')
                     -> limit(20)
                     ->select();
     }
 
     /*
+     * @getPerfectCoupon() 精选好券列表
+     * */
+    public function getPerfectCoupon(){
+        return $this ->field('id,goods_id,name,banner,price,type,sales,coupon_money')
+            -> order('commission DESC')
+            -> limit(1,200)
+            ->select();
+    }
+
+
+    /*
      * @getIndexHotCoupon() 首页爆款好券20个
      * */
     public function getIndexHotCoupon(){
-        return $this ->field('id,goods_id,name,banner,price,coupon_money')
+        return $this ->field('id,goods_id,name,banner,price,type,sales,coupon_money')
             -> order('sales DESC')
             -> limit(20)
+            ->select();
+    }
+
+    /*
+     * @getHotCoupon() 爆款好券列表
+     * */
+    public function getHotCoupon(){
+        return $this ->field('id,goods_id,name,banner,price,sales,type,coupon_money')
+            -> order('sales DESC')
+            -> limit(1,200)
             ->select();
     }
 
@@ -42,7 +68,7 @@ class Goods extends Model
      * @getIndexListCoupon() 首页推荐好券50个
      * */
     public function getIndexListCoupon(){
-        return $this ->field('id,goods_id,name,banner,price,coupon_money,sales')
+        return $this ->field('id,goods_id,name,banner,price,coupon_money,sales,type,sales')
             //-> order('sales DESC')
             -> limit(50)
             ->select();
@@ -53,7 +79,7 @@ class Goods extends Model
     *  $id     产品goods_id
     * */
     public function getGoodsDetailsById($id){
-        return $this -> field('id,goods_id,name,banner,price,coupon_money,sales,commission,coupon_url,coupon_extend')
+        return $this -> field('id,goods_id,name,banner,detail_url,price,type,tao_url,coupon_money,sales,commission,coupon_url,coupon_extend')
                     ->where('goods_id',$id)
                     ->find();
     }
@@ -65,17 +91,90 @@ class Goods extends Model
     * */
     public function getRecommendGoodsById($id){
         $class = $this->getClassById($id);
-        return $this-> alias('l')
-            ->field('id,goods_id,name,banner,price,coupon_money')
-            ->where('class',$class['class'])
-            ->order('commission DESC')
-            ->limit(rand(1,50),8)
-            ->select();
+        return $this -> field('id,goods_id,name,banner,price,type,sales,coupon_money')
+                    -> where('class',$class['class'])
+                    -> order('commission DESC')
+                    -> limit(rand(1,50),8)
+                    -> select();
     }
     //@ getClassById 辅助于getRecommendGoodsById
     protected function getClassById($id){
         return $this-> field('class') -> where('goods_id',$id)->find();
     }
+
+
+
+    /*
+     * @getGoodsListByTime() 今日优惠券列表
+     * */
+    public function getGoodsListByTime(){
+        return $this -> field('id,goods_id,name,banner,price,type,sales,coupon_money')
+                    -> order('coupon_start DESC')
+                    -> limit(1,200)
+                    -> select();
+    }
+
+    /*
+     * @getGoodsListByTmall() 天猫优惠券列表
+     * */
+    public function getGoodsListByTmall(){
+        return $this -> field('id,goods_id,name,banner,price,type,sales,coupon_money')
+                    -> where('type','天猫')
+                    -> limit(1,200)
+                    -> select();
+    }
+
+
+    /*
+     * @getGoodsListByCouponMoney() 大额优惠券列表
+     * */
+    public function getGoodsListByCouponMoney(){
+        return $this -> field('id,goods_id,name,banner,price,type,sales,coupon_money')
+                    -> order('coupon_money DESC')
+                    -> limit(1,200)
+                    -> select();
+    }
+
+    /*
+     * @getGoodsListByNine() 9.9优惠券列表
+     * */
+    public function getGoodsListByNine(){
+        return $this -> field('id,goods_id,name,banner,price,type,sales,coupon_money')
+                    ->where('(price-coupon_money) < 10')
+                    -> limit(1,200)
+                    -> select();
+    }
+
+    /*
+     * @getGoodsListByRatio() 高奖金优惠券列表
+     * */
+    public function getGoodsListByRatio(){
+        return $this -> field('id,goods_id,name,banner,price,type,sales,coupon_money')
+            -> order('ratio DESC')
+            -> limit(1,200)
+            -> select();
+    }
+
+    /*
+     * @getCouponSquare() 广场
+     * */
+    public function getCouponSquare(){
+        return $this -> field('id,goods_id,name,banner,price,type,sales,coupon_money')
+            -> limit(rand(1,500),500)
+            -> select();
+    }
+
+    /*
+     * @getCouponNav() 导航优惠券
+     * */
+    public function getCouponNav($nav){
+        return $this -> field('id,goods_id,name,banner,price,type,sales,coupon_money')
+            -> where('class',$nav)
+            -> limit(1,200)
+            -> select();
+    }
+
+
 
 
 }
